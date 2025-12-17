a205e6741597f0afa908e8264af9c3232f338cfa
Mohamed Helmy <Mohamedlegendary2005@gmail.com>
2025-12-17 04:47:53 +0200

    3 - Created Flutter mic loopback app using flutter create with proper structure and functionality

--- flutter_mic_loopback/android/app/src/main/kotlin/com/example/flutter_mic_loopback/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.flutter_mic_loopback
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ mic_loopback_app/android/app/src/main/kotlin/com/example/mic_loopback_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.mic_loopback_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
